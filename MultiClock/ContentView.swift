e6df830a456ce58a8af940f079c992aac0a613fd
Michael Olson <mao@olsons.net>
2023-10-01 09:36:06 -0700

    Initial Commit

---
+++ MultiClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiClock
+//
+//  Created by Mike Olson on 10/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiClock/MultiClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiClockApp.swift
+//  MultiClock
+//
+//  Created by Mike Olson on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
