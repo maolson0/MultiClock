cade663ddf26ae1b14623453e2eae8d3c46ff8bc
Michael Olson <mao@olsons.net>
2023-10-13 15:32:13 -0700

    First version with a working time conversion tab. Still needs some clean-up but want to commit this as a base.

---
+++ MultiClock/ConverterView.swift
@@ -0,0 +1,18 @@
+//
+//  ConverterView.swift
+//  MultiClock
+//
+//  Created by Mike Olson on 10/9/23.
+//
+
+import SwiftUI
+
+struct ConverterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConverterView()
+}
